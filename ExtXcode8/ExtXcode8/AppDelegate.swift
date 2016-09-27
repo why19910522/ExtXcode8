7b5a133e28fb3bfa0e8520c878f64924bd048fd7
why19910522 <947962910@qq.com>
2016-09-27 11:47:39 +0800

    创建项目

---
+++ ExtXcode8/ExtXcode8/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ExtXcode8
+//
+//  Created by 王洪运 on 2016/9/27.
+//  Copyright © 2016年 王洪运. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
